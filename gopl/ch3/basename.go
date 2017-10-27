6084b682ca7886d1f79b49470edcf7cd234c7c78
Humbertzhang <504490160@qq.com>
2017-10-27 14:02:28 +0800

    gopl cha3

---
+++ gopl/ch3/basename.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+    s := ""
+    fmt.Print("Input a string:")
+    fmt.Scanf("%s", &s)
+    s2 := basename(s)
+    fmt.Printf("Basename: %s\n", s2)
+}
+
+func basename(s string) string {
+    start := 0
+    end := len(s)
+    for i := range s{
+        if s[i] == '/'{
+            start = i+1
+        }else if s[i] == '.'{
+            end = i
+        }
+    }
+    s2 := s[start:end]
+    return s2
+}
